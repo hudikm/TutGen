024ef24ec0c83d78463716d7daeff77d5d62bc76
Martin Hudik <hudikm@gmail.com>
2020-04-08 15:09:28 +0200

    oprava name parametra

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='PatchCreator',
+    name='TutGen',
     url='https://github.com/hudikm/tutgen',
     author='Martin Hudik',
     author_email='martin.hudik@fri.uniza.sk',
