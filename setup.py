e2e10474ed578038aa5078e4ecff203a116b7c0f
Martin Hudik <hudikm@gmail.com>
2020-04-17 10:02:58 +0200

    fix data_file relative path

--- setup.py
+++ setup.py
@@ -26,5 +26,8 @@ setup(
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
     scripts=['scripts/tutGen.py'],
-    data_files=[(str(pathlib.Path.home().joinpath('.local/TutGen/templates')),['templates/files_list.jinja', 'templates/gen_tags.jinja','templates/mkdocs.jinja','templates/mkdocs_obsah.jinja'])],
+    # data_files use relative paths(user or system wide):
+    # sys.prefix -> /usr/local/
+    # sys.USER_DEFINED -> linux  ~/.local/ or win %APPDATA%\Python
+    data_files=[('/TutGen/templates',['templates/files_list.jinja', 'templates/gen_tags.jinja','templates/mkdocs.jinja','templates/mkdocs_obsah.jinja'])],
 )
